0a709d4b2d0337db87da7c5d36e14c4b955bdb23
Dev <hoangtv1809@gmail.com>
2025-01-10 22:08:48 +0700

    cmt ui

--- src/App.js
+++ src/App.js
@@ -3,37 +3,66 @@ import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import DefaultLayout from '~/layouts';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        const Page = route.component;
+        // <Router>
+        //     <div className="App">
+        //         <Routes>
+        //             {publicRoutes.map((route, index) => {
+        //                 const Page = route.component;
 
-                        let Layout = DefaultLayout;
+        //                 let Layout = DefaultLayout;
 
-                        if (route.layout) {
-                            Layout = route.layout;
-                        } else if (route.layout === null) {
-                            Layout = Fragment;
-                        }
+        //                 if (route.layout) {
+        //                     Layout = route.layout;
+        //                 } else if (route.layout === null) {
+        //                     Layout = Fragment;
+        //                 }
 
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
-            </div>
-        </Router>
+        //                 return (
+        //                     <Route
+        //                         key={index}
+        //                         path={route.path}
+        //                         element={
+        //                             <Layout>
+        //                                 <Page />
+        //                             </Layout>
+        //                         }
+        //                     />
+        //                 );
+        //             })}
+        //         </Routes>
+        //     </div>
+        // </Router>
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={4}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+            <SwiperSlide>Slide 10</SwiperSlide>
+            <SwiperSlide>Slide 11</SwiperSlide>
+            <SwiperSlide>Slide 12</SwiperSlide>
+            <SwiperSlide>Slide 13</SwiperSlide>
+            <SwiperSlide>Slide 14</SwiperSlide>
+            ...
+        </Swiper>
     );
 }
 
